75c327d6a8539ecce837c6f2d6cca0daa2b2a58a
Oleksii Bukhanevych <albukh171@gmail.com>
2022-02-14 20:57:13 +0200

    4

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,10 @@
-const counterValue = 0;
 
-const incButton = document.querySelector(`button[data-action="increment"]`)
-incButton.addEventListener(`click`, (counterValue) => { `${counterValue} + 1`} )
 
-console.log(counterValue)
+
+let plusBtn = document.querySelector(`button[data-action="increment"]`)
+let minusBtn = document.querySelector(`button[data-action="decrement"]`)
+let currentValue = document.querySelector(`#value`);
+let counterValue = 0;
+plusBtn.addEventListener(`click`, () => {counterValue += 1; currentValue.textContent = counterValue;} );
+minusBtn.addEventListener(`click`, () => {counterValue -= 1; currentValue.textContent = counterValue;} );
+
