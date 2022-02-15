6e660b86e9236ebdc5cd57b9de3fa6d10b47438d
Oleksii Bukhanevych <albukh171@gmail.com>
2022-02-15 09:29:23 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeButtonRef = document.querySelector(`.widget > button`);
+const colorInfoRef = document.querySelector(`.color`);
+const theBody = document.body;
+
+changeButtonRef.addEventListener('click', () => {
+  theBody.style.backgroundColor = getRandomHexColor();
+  colorInfoRef.textContent = getRandomHexColor();
+});
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
